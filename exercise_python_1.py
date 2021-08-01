371008201695e04c032aece46398f02289cc9503
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-01 19:49:12 -0400

    Update exercise_python_1.py

--- exercise_python_1.py
+++ exercise_python_1.py
@@ -31,11 +31,10 @@ print(first_name+fun_fact+ my_initials+ first_initial+ second_initial )
 # Loveseats for Neat Suites on Fleet Street. With our newfound knowledge of Python programming, we’re going to build a 
 # system to help speed up the process of creating receipts for your customers.
 
-
 lovely_loveseat_description = '''
 Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.
 '''
-lovely_loveseat_price  = 254.00
+lovely_loveseat_price = 254.00
 
 stylish_settee_description  = '''
 Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.
@@ -47,3 +46,22 @@ Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.
 '''
 luxurious_lamp_price = 52.15
 sales_tax = 0.088
+
+customer_one_total = 0
+customer_one_itemization = ""
+
+customer_one_total += lovely_loveseat_price
+customer_one_itemization += lovely_loveseat_description
+
+customer_one_total += luxurious_lamp_price
+customer_one_itemization += luxurious_lamp_description
+
+customer_one_tax = customer_one_total*sales_tax
+
+customer_one_total += customer_one_tax
+
+print("Customer One Items:")
+print(customer_one_itemization)
+
+print("Customer One Total:")
+print(customer_one_total)
