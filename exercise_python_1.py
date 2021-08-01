bf4f41f51d02928b3746b8de7c40419d9bf8ac7c
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-01 18:14:05 -0400

    Update exercise_python_1.py

--- exercise_python_1.py
+++ exercise_python_1.py
@@ -1,11 +1,26 @@
 
-#First, write two comments with: Your first and last name and a fun fact about yourself
+#1. First, write two comments with: Your first and last name and a fun fact about yourself
+#2. Output your first initial as a block letter. There are a few ways to do this! Press Save to run your program
+#3. Output your second initial as a block letter by adding to the print() statements.
+#4 .Press Save to run your program.
 
+first_name = 'Hi! I am William Roberts and '
+fun_fact = 'I love Harry Potter'
+my_initials = ' and my initials are:'
 
+first_initial = '''
+W            w
+ W    ww    w
+  W  w  w  w
+   Ww     w
+'''
 
-#Output your first initial as a block letter. There are a few ways to do this! Press Save to run your program.
-
-
-#Output your second initial as a block letter by adding to the print() statements.
-
-#Press Save to run your program.
+second_initial = '''
+R R R R            
+R     R  
+R    R   
+R  R
+R   R 
+R     R
+'''
+print(first_name+fun_fact+ my_initials+ first_initial+ second_initial )
