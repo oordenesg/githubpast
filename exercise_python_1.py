61d892df86b407443dc89a7e4babb205dcbdf481
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-01 18:13:27 -0400

    Create exercise_python_1.py

---
+++ exercise_python_1.py
@@ -0,0 +1,11 @@
+
+#First, write two comments with: Your first and last name and a fun fact about yourself
+
+
+
+#Output your first initial as a block letter. There are a few ways to do this! Press Save to run your program.
+
+
+#Output your second initial as a block letter by adding to the print() statements.
+
+#Press Save to run your program.
