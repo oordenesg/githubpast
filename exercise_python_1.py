cc1c8f5bea106247dbb95053cdbcefbae0451800
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-02 19:46:27 -0400

    Update exercise_python_1.py

--- exercise_python_1.py
+++ exercise_python_1.py
@@ -31,19 +31,28 @@ print(first_name+fun_fact+ my_initials+ first_initial+ second_initial )
 # Loveseats for Neat Suites on Fleet Street. With our newfound knowledge of Python programming, we’re going to build a 
 # system to help speed up the process of creating receipts for your customers.
 
+
+# 1. Let’s add in our first item, the Lovely Loveseat that is the store’s namesake. Create a variable called lovely_loveseat_description and assign to it the following string:
+
 lovely_loveseat_description = '''
 Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.
 '''
+# 2. Now let’s create a price for the loveseat. Create a variable lovely_loveseat_price and set it equal to 254.00.
 lovely_loveseat_price = 254.00
 
+# 3. Let’s extend our inventory with another characteristic piece of furniture! Create a variable called stylish_settee_description and assign to it the following string:
 stylish_settee_description  = '''
 Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.
 '''
+# 4. Now let’s set the price for our Stylish Settee. Create a variable stylish_settee_price and assign it the value of 180.50.
 stylish_settee_price = 180.50
 
+# 5. We just need one more item before we’re ready for business. Create a new variable called luxurious_lamp_description and assign it the following:
 luxurious_lamp_description = '''
 Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.
 '''
+
+# 6. Let’s set the price for this item. Create a variable called luxurious_lamp_price and set it equal to 52.15.
 luxurious_lamp_price = 52.15
 sales_tax = 0.088
 
