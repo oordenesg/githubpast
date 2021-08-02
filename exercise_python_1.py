1f86971edb33e1feb3eb8e67b1331c3769ef093c
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-02 19:54:06 -0400

    Update exercise_python_1.py

--- exercise_python_1.py
+++ exercise_python_1.py
@@ -54,19 +54,36 @@ Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.
 
 # 6. Let’s set the price for this item. Create a variable called luxurious_lamp_price and set it equal to 52.15.
 luxurious_lamp_price = 52.15
+
+
+# 7. In order to be a business, we should also be calculating sales tax. Let’s store that in a variable as well. Define the variable sales_tax and set it equal to .088. That’s 8.8%.
 sales_tax = 0.088
 
+# 8 Our first customer is making their purchase! Let’s keep a running tally of their expenses by defining a variable called customer_one_total. Since they haven’t purchased anything yet, let’s set that variable equal to 0 for now.
 customer_one_total = 0
+
+
+# 9. We should also keep a list of the descriptions of things they’re purchasing. Create a variable called customer_one_itemization and set that equal to the empty string "". We’ll tack on the descriptions to this as they make their purchases.
 customer_one_itemization = ""
 
+# 10. Our customer has decided they are going to purchase our Lovely Loveseat! Add the price to customer_one_total.
 customer_one_total += lovely_loveseat_price
+
+
+# 11. Let’s start keeping track of the items our customer purchased. Add the description of the Lovely Loveseat to customer_one_itemization.
 customer_one_itemization += lovely_loveseat_description
 
+# 12. Our customer has also decided to purchase the Luxurious Lamp! Let’s add the price to the customer’s total.
+
 customer_one_total += luxurious_lamp_price
+
+# 13. Let’s keep the itemization up-to-date and add the description of the Luxurious Lamp to our itemization.
 customer_one_itemization += luxurious_lamp_description
 
+# 14. They’re ready to check out! Let’s begin by calculating sales tax. Create a variable called customer_one_tax and set it equal to customer_one_total times sales_tax.
 customer_one_tax = customer_one_total*sales_tax
 
+# 15. Add the sales tax to the customer’s total cost.
 customer_one_total += customer_one_tax
 
 print("Customer One Items:")
