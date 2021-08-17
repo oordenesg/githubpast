76be866d244b0ca0d78435eb3f08704f2479a705
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-17 13:27:05 -0400

    Update Lens_slice.py

--- Lens_slice.py
+++ Lens_slice.py
@@ -15,3 +15,31 @@
 
 
 # 4. Find the length of the toppings list and store it in a variable called num_pizzas.
+
+
+# 5. Print the string We sell [num_pizzas] different kinds of pizza!, where [num_pizzas] represents the value of our variable num_pizzas.
+
+
+# 6. Convert our toppings and prices lists into a two-dimensional list called pizza_and_prices that has the following associated values.
+
+
+# 7. Print pizza_and_prices. Does it look the way you expect?
+
+# 8. Sort pizza_and_prices so that the pizzas are in the order of increasing price (ascending).
+
+
+# 9. Store the first element of pizza_and_prices in a variable called cheapest_pizza.
+
+
+# 10. A man walks into the pizza store and shouts “I will have your MOST EXPENSIVE pizza!” Get the last item of the pizza_and_prices list and store it in a variable called priciest_pizza
+
+
+# 11. It looks like the most expensive pizza from the previous step was our very last "anchovies" slice. Remove it from our pizza_and_prices list since the man bought the last slice.
+
+# 12. Since there is no longer an "anchovies" pizza, you want to add a new topping called "peppers" to keep your customers excited about new toppings. Here is what your new topping looks like:
+# [2.5, "peppers"] Add the new peppers pizza topping to our list pizza_and_prices.
+
+
+#13. Three mice walk into the store. They don’t have much money (they’re mice), but they do each want different pizzas. 
+# Slice the pizza_and_prices list and store the 3 lowest cost pizzas in a list called three_cheapest. Print the three_cheapest list.
+
