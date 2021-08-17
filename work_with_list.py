567d45d4225aae03897a45b55d055e3062c7514e
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-17 11:41:59 -0400

    Create work_with_list.py

---
+++ work_with_list.py
@@ -0,0 +1,12 @@
+# In this lesson, we learned how to:
+# 1. Add elements to a list by index using the .insert() method.
+# 2. Remove elements from a list by index using the .pop() method.
+# 3. Generate a list using the range() function.
+# 4. Get the length of a list using the len() function.
+# 5. Select portions of a list using slicing syntax.
+# 6. Count the number of times that an element appears in a list using the .count() method.
+# 7. Sort a list of items using either the .sort() method or sorted() function.
+
+
+
+
