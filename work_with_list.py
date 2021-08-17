8fa9a8b31c31ae9fb04c32f814219b18bd49cd66
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-17 11:49:55 -0400

    Update work_with_list.py

--- work_with_list.py
+++ work_with_list.py
@@ -8,5 +8,25 @@
 # 7. Sort a list of items using either the .sort() method or sorted() function.
 
 
+# Our friend Jiho has been so successful in both the flower and grocery business that she has decided to open a furniture store. 
+# Jiho has compiled a list of inventory items into a list called inventory and wants to know a few facts about it.
+# First, how many items are in the warehouse? Save the answer to a variable called inventory_len.
+
+# Select the first element in inventory. Save it to a variable called first.
 
 
+# Select the last element from inventory. Save it to a variable called last.
+
+
+# Select items from the inventory starting at index 2 and up to, but not including, index 6. Save your answer to a variable called inventory_2_6.
+
+# Select the first 3 items of inventory. Save it to a variable called first_3.
+
+# How many 'twin bed's are in inventory? Save your answer to a variable called twin_beds.
+
+
+# Remove the 5th element in the inventory. Save the value to a variable called removed_item.
+
+# There was a new item added to our inventory called "19th Century Bed Frame". Use the .insert() method to place the new item as the 11th element in our inventory
+
+# Sort inventory using the .sort() method or the sorted() function. Print inventory to see the result.
