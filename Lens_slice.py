d77457fa560311c142737a359c0a4fb1d7a60795
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-08-17 12:00:01 -0400

    Create Lens_slice.py

---
+++ Lens_slice.py
@@ -0,0 +1,17 @@
+
+# You work at Len’s Slice, a new pizza joint in the neighborhood. You are going to use your knowledge of Python lists to organize some of your sales data
+
+
+# 1. To keep track of the kinds of pizzas you sell, create a list called toppings that holds the following: 
+# "pepperoni", "pineapple","cheese","sausage", "olives", "anchovies" ,"mushrooms"
+
+
+# 2. To keep track of how much each kind of pizza slice costs, create a list called prices that holds the following integer values:
+# 2,6,1,3,2,7,2
+
+
+# 3. Your boss wants you to do some research on $2 slices.
+# Count the number of occurrences of 2 in the prices list, and store the result in a variable called num_two_dollar_slices. Print it out.
+
+
+# 4. Find the length of the toppings list and store it in a variable called num_pizzas.
