bb9226fcfaf09f8cb9a8346eb5c9a68d79fa30d8
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-09-09 19:58:56 -0300

    Update carlys_clippers.py

--- carlys_clippers.py
+++ carlys_clippers.py
@@ -32,3 +32,47 @@ average_price = total_price/len(prices)
 # 4. Print the value of average_price so the output looks like:
 
 print('Average Haircut Price: ' + str(average_price))
+
+
+# 5. That average price is more expensive than Carly thought it would be! She wants to cut all prices by 5 dollars. Use a list comprehension to make a list called new_prices, which has each element in prices minus 5.
+
+new_prices = []
+
+for price in prices:
+  new_prices.append(price-5)
+  
+  
+# 6. Print new_prices.
+
+print(new_prices)
+
+# 7. Carly really wants to make sure that Carly’s Clippers is a profitable endeavor. She first wants to know how much revenue was brought in last week. Create a variable called total_revenue and set it to 0.
+
+total_revenue = 0
+
+
+# 8. Use a for loop to create a variable i that goes from 0 to len(hairstyles). Add the product of prices[i] (the price of the haircut at position i) and last_week[i] (the number of people who got the haircut at position i) to total_revenue at each step.
+
+total_revenue = 0
+
+for i in range(0,len(hairstyles)):
+  total_revenue += prices[i]*last_week[i]
+  
+# 9. After your loop, print the value of total_revenue, so the output looks like:
+
+print('Total Revenue: '+ str(total_revenue))
+
+
+# 10. Find the average daily revenue by dividing total_revenue by 7. Call this number average_daily_revenue and print it out.
+
+average_daily_revenue  = total_revenue/7
+print(average_daily_revenue)
+
+# 11. Carly thinks she can bring in more customers by advertising all of the haircuts she has that are under 30 dollars. Print cuts_under_30.
+
+cuts_under_30  = []
+for i in range(0,len(new_prices)-1):
+  if prices[i] < 30:
+    cuts_under_30.append(hairstyles[i])
+
+print(cuts_under_30)
