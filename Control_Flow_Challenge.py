7eed4d05cdebaac068ad763f4e6f0f5dcc2664ce
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-09-17 11:52:19 -0300

    Update Control_Flow_Challenge.py

--- Control_Flow_Challenge.py
+++ Control_Flow_Challenge.py
@@ -132,3 +132,70 @@ print(max_num(-10, 0, 10)) # should print 10
 print(max_num(-10, 5, -30)) # should print 5
 print(max_num(-5, -10, -10)) # should print -5
 print(max_num(2, 3, 3)) # should print "It's a tie!"
+
+# 11. Create a function called append_size that has one parameter named lst.
+# The function should append the size of lst (inclusive) to the end of lst. The function should then return this new list.
+# For example, if lst was [23, 42, 108], the function should return [23, 42, 108, 3] because the size of lst was originally 3.
+
+def append_size(lst):
+  new_list = lst.append(len(lst))
+  return lst
+
+print(append_size([23, 42, 108])) # should print [23, 42, 108, 3]
+
+# 12. Write a function named append_sum that has one parameter — a list named named lst.
+# The function should add the last two elements of lst together and append the result to lst. It should do this process three times and then return lst.
+# For example, if lst started as [1, 1, 2], the final result should be [1, 1, 2, 3, 5, 8].
+
+ef append_sum(lst):
+  new_list = lst
+  for i in range(0,3):
+    last_element = sum(lst[-2:])
+    new_list.append(last_element)
+  return new_list
+
+print(append_sum([1, 1, 2])) # should print [1, 1, 2, 3, 5, 8]
+
+# 13. Write a function named larger_list that has two parameters named lst1 and lst2.
+# The function should return the last element of the list that contains more elements. If both lists are the same size, then return the last element of lst1.
+
+def larger_list(lst1,lst2):
+  if len(lst1) > len(lst2):
+    return lst1[-1]
+  elif len(lst2) > len(lst1):
+    return lst2[-1]
+  else:
+    return lst1[-1]
+
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10])) # should print 5
+
+# 14. Create a function named more_than_n that has three parameters named lst, item, and n.
+# The function should return True if item appears in the list more than n times. The function should return False otherwise.
+
+def more_than_n(lst,item,n):
+  count = 0
+  for i in range(0,len(lst)):
+    if item == lst[i]:
+      count += 1
+  if count > n:
+    return True
+  else:
+    return False
+  
+print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3)) # should print True
+
+# second option
+
+def more_than_n(lst, item, n):
+  if lst.count(item) > n:
+    return True
+  else:
+    return False
+
+# 15. Write a function named combine_sort that has two parameters named lst1 and lst2.
+# The function should combine these two lists into one new list and sort the result. Return the new sorted list.
+
+def combine_sort(lst1,lst2):
+  return sorted(lst1+lst2)
+
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10])) # should print [-10, 2, 2, 4, 5, 5, 10, 10]
