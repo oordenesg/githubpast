3871f42a6478de7eaa39d0d88deb02e1ee993379
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-10-25 21:49:41 -0300

    Update Hacking_The_Fender.py

--- Hacking_The_Fender.py
+++ Hacking_The_Fender.py
@@ -4,8 +4,12 @@
 
 # 1. First import the CSV module, since we’ll be needing it to parse the data.
 # We need to create a list of users whose passwords have been compromised, create a new list and save it to the variable compromised_users. 
-
+import csv
+compromised_users = []
 
 # 2. Next we’ll need you to open up the file itself. Store it in a file object called password_file.
 # Pass the password_file object holder to our CSV reader for parsing. Save the parsed csv.DictReader object as password_csv.
+with open("passwords.txt") as password_file:
+  password_csv = csv.DictReader(password_file)
+
 
