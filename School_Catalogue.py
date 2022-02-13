3185c2d296518b8b4707a87dc1e64265c01780ea
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2022-02-13 20:56:14 -0300

    Update School_Catalogue.py

--- School_Catalogue.py
+++ School_Catalogue.py
@@ -19,3 +19,49 @@
 # 4. High
 #Includes everything in the School class, plus one additional property
 #Properties: sportsTeams (list of strings, like ['basketball', 'tennis'])
+
+
+
+class School:
+  def __init__(self,name,level,numberOfStudents):
+    self.name = name
+    self.level = level
+    self.numberOfStudents = numberOfStudents
+
+  def get_name(self):
+    return self.name
+  def get_level(self):
+    return self.level
+  def get_numberofstudents(self):
+    return self.numberOfStudents
+
+  def set_numberOfStudents(self,new_numberOfStudents):
+    self.numberOfStudents = new_numberOfStudents
+
+  def __repr__(self):
+    return f'A {self.level} school named {self.name} with {self.numberOfStudents} students'
+
+
+class PrimarySchool(School):
+  def __init__(self,name,numberOfStudents,pickupPolicy):
+    super().__init__(name,"primary",numberOfStudents)
+    self.pickupPolicy = pickupPolicy
+    
+  def get__primarySchool(self):
+    return self.pickupPolicy
+
+  def __repr__(self):
+    parentRepr = super().__repr__()
+    return parentRepr + "The pickup polity is {pickupPolicy}".format(pickupPolicy = self.pickupPolicy)
+
+class HighSchool(School):
+  def __init__(self,name,numberOfStudents,sportsTeams):
+    super().__init__(name,'high',numberOfStudents)
+    self.sportsTeams = sportsTeams
+
+  def get_highSchool(self):
+    return self.sportsTeams
+  
+  def __repr__(self):
+    return f"These are the sports {self.sportsTeams}"
+
