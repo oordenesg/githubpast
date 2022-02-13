7b4629101a053ded396b9e29222af5d30f29c29c
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2022-02-13 20:55:49 -0300

    Create School_Catalogue.py

---
+++ School_Catalogue.py
@@ -0,0 +1,21 @@
+## School Catalogue
+
+# We need to create classes for primary and high schools. Because these classes share properties and methods, 
+# each will inherit from a parent School class. Our parent and three child classes have the following properties, getters, setters, and methods:
+
+# 1. School
+# Properties: name (string), level (one of three strings: 'primary', 'middle', or 'high'), and numberOfStudents (integer)
+# Getters: all properties have getters
+# Setters: the numberOfStudents property has a setter
+# Methods: A __repr__ method that displays information about the school.
+
+# 2. Primary
+# Includes everything in the School class, plus one additional property
+# Properties: pickupPolicy (string, like "Pickup after 3pm")
+
+# 3. Middle
+#Does not include any additional properties or methods
+
+# 4. High
+#Includes everything in the School class, plus one additional property
+#Properties: sportsTeams (list of strings, like ['basketball', 'tennis'])
